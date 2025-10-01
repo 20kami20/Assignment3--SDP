3ecc909627a4e7794f18c9a263b0bb362523f198
20kami20 <kuchitarova15@gmail.com>
2025-10-01 16:11:57 +0300

    Update AudioPlayer.java

--- AudioPlayer.java
+++ AudioPlayer.java
@@ -1,15 +1,6 @@
 public class AudioPlayer implements MediaPlayer {
-    private MediaAdapter mediaAdapter;
-
-    @Override
-    public void play(String audioType, String fileName) {
-        if (audioType.equalsIgnoreCase("mp3")) {
-            System.out.println("Playing MP3 file: " + fileName);
-        } else if (audioType.equalsIgnoreCase("mp4") || audioType.equalsIgnoreCase("avi")) {
-            mediaAdapter = new MediaAdapter(audioType);
-            mediaAdapter.play(audioType, fileName);
-        } else {
-            System.out.println("Invalid media type: " + audioType + " not supported.");
-        }
+    public void play(String type, String file) {
+        if(type.equalsIgnoreCase("mp3")) System.out.println("Playing MP3: " + file);
+        else new MediaAdapter(type).play(type, file);
     }
 }
